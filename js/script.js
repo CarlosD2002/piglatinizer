b984b131ec2f3e5ea31d23b456dc4612424f695f
CarlosD2002 <c.wuchen@ihsph.org>
2019-10-29 22:13:09 +0000

    Fix the 2 word problem

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-
-
-
-  
-  
-  
-  
+function pigLatin(text) {
+    return text.substr(1)+text.charAt(0)+"ay";
+}
+$("#button").click(function() {
+let text =
+$(".input").val();
+$(".output").text(function() {
+return pigLatin(text);
+});
+});
 });
